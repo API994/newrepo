ad3f45f68a3e6f71418dd52a4f541d8f7f8ef630
Bhumika <bhumika.motwani@apisero.com>
2021-02-22 14:13:45 +0530

    next commit

---
+++ Age.java
@@ -0,0 +1,10 @@
+public class Age {  
+public static void main(String[] args) {    
+    int age=20;   
+    if(age>=18){  
+        System.out.print("You can vote");  
+    else{  
+        System.out.print("You cannot vote");  
+    }  
+}  
+}  
