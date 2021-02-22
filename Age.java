ac4e8096b59205f92807cb7c8d0ba89be4e1168d
Bhumika <bhumika.motwani@apisero.com>
2021-02-22 14:21:10 +0530

    I am 3 build

--- Age.java
+++ Age.java
@@ -2,7 +2,8 @@ public class Age {
 public static void main(String[] args) {    
     int age=20;   
     if(age>=18){  
-        System.out.print("You can vote");  
+        System.out.print("You can vote"); 
+      } 
     else{  
         System.out.print("You cannot vote");  
     }  
